00d81d41dc5a99bae809e5593df3b151d5d33807
Priyaranjan1993 <priyaranjan9090@gmail.com>
2018-09-19 17:27:15 -0400

    Shortest word distance

---
+++ src/LinkedList/ShortestWordDistance.java
@@ -0,0 +1,35 @@
+package LinkedList;
+
+public class ShortestWordDistance {
+
+	public int shortestDistance(String[] words, String word1, String word2) {
+		int w1 = -1;
+		int w2 = -1;
+		int minDistance = words.length;
+
+		for(int i=0; i < words.length; i++)
+		{
+			if(words[i] == word1)
+			{
+				w1 = i;
+			}
+
+			else if(words[i] == word2)
+			{
+				w2 = i;
+			}
+
+			if(w1 != -1 && w2 != -1)
+				minDistance = Math.min(minDistance, Math.abs(w1-w2));
+		}
+		return minDistance;
+	}
+
+	public static void main(String args[])
+	{
+		ShortestWordDistance s = new ShortestWordDistance();
+		String[] arr = new String[] {"abc","def","practice", "makes", "perfect", "coding", "makes"};
+		int distance = s.shortestDistance(arr, "coding", "practice");
+		System.out.println(distance);
+	}
+}
