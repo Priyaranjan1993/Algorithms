af6922c02aab4f52e06975ce6ec69ce3df7a4343
Priyaranjan1993 <priyaranjan9090@gmail.com>
2018-10-13 22:26:08 -0400

    commit of validate a BST

--- src/Tree/ValidateBST.java
+++ src/Tree/ValidateBST.java
@@ -1,5 +1,51 @@
 package Tree;
 
-public class ValidateBST {
+import java.util.ArrayList;
+import java.util.List;
 
+public class ValidateBST {
+	TreeNode root;
+	List<Integer> ls = new ArrayList<>();
+	int count = 0;
+	Boolean b = true;
+	public boolean isValidBST(TreeNode root)
+	{
+		inOrder(root);
+		return b;
+	}
+
+	private void inOrder(TreeNode root) {
+		if(root == null)
+		{
+			return;
+		}
+		inOrder(root.left);
+		ls.add(root.val);
+		if(count != 0 && root.val <= ls.get(count -1))
+		{
+			b = false;
+		}
+		count++;
+		inOrder(root.right);
+	}
+
+
+	public static void main(String args[])
+	{
+		ValidateBST tree = new ValidateBST();
+
+/*		tree.root = new TreeNode(5);
+		tree.root.left = new TreeNode(1);
+
+		tree.root.right = new TreeNode(4);
+		tree.root.right.left = new TreeNode(3);
+		tree.root.right.right = new TreeNode(6);*/
+		
+		tree.root = new TreeNode(2);
+		tree.root.left = new TreeNode(1);
+		tree.root.right = new TreeNode(3);
+		
+		Boolean b = tree.isValidBST(tree.root);
+		System.out.println(b);
+	}
 }
