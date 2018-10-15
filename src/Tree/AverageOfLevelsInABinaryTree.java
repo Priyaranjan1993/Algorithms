49b5e1ea4a4d8ee79859a643f870bada72053061
Priyaranjan1993 <priyaranjan9090@gmail.com>
2018-10-15 00:19:53 -0400

    commit of Average of Levels in a Binary tree

---
+++ src/Tree/AverageOfLevelsInABinaryTree.java
@@ -0,0 +1,65 @@
+package Tree;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class AverageOfLevelsInABinaryTree {
+	TreeNode root;
+
+	public List<Double> averageOfLevels(TreeNode root) {
+		List<Integer> count = new ArrayList<Integer>();
+		List<Double> sum = new ArrayList<Double>();
+		List<Double> result = new ArrayList<Double>();
+		averageCalculation(root, 0, count, sum);
+		for(int i =0 ; i< sum.size(); i++)
+		{
+			result.add((double) (sum.get(i)/count.get(i)));
+		}
+		return result;
+	}
+
+	public void averageCalculation(TreeNode root, int i, List<Integer> count, List<Double> sum) {
+		if(root == null)
+		{
+			return;
+		}
+		if(i < sum.size())
+		{
+			sum.set(i, sum.get(i)+root.val);
+			count.set(i,count.get(i)+1);
+		}
+		else {
+			sum.add(1.0 * root.val);
+			count.add(1);
+		}
+		averageCalculation(root.left, i+1, count, sum);
+		averageCalculation(root.right, i+1, count, sum);
+	}
+
+	public static void main(String args[])
+	{
+		AverageOfLevelsInABinaryTree tree = new AverageOfLevelsInABinaryTree();
+/*		tree.root = new TreeNode(5);
+		tree.root.left = new TreeNode(3);
+		tree.root.left.left = new TreeNode(2);
+		tree.root.left.left.left = new TreeNode(1);
+
+		tree.root.right = new TreeNode(6);
+		tree.root.left.right = new TreeNode(4);*/
+		
+		tree.root = new TreeNode(3);
+		tree.root.left = new TreeNode(9);
+		tree.root.left.left = new TreeNode(15);
+		
+		tree.root.right = new TreeNode(20);
+		tree.root.left.right = new TreeNode(7);
+		
+		
+		List<Double> result = new ArrayList<Double>();
+		result = tree.averageOfLevels(tree.root);
+		for(Double d : result)
+		{
+			System.out.println(d);
+		}
+	}
+}
