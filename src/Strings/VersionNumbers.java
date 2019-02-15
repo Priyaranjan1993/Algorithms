2d30b07d8ee91f5bdb8ed127a6fad285f443d536
Priyaranjan1993 <priyaranjan9090@gmail.com>
2019-02-14 23:54:29 -0500

    version numbers

---
+++ src/Strings/VersionNumbers.java
@@ -0,0 +1,28 @@
+package Strings;
+
+public class VersionNumbers {
+    public int compareVersion(String version1, String version2) {
+        String[] v1 = version1.split("\\.");
+        String[] v2 = version2.split("\\.");
+        
+        int longest = v1.length > v2.length? v1.length: v2.length;
+        
+        for(int i=0; i<longest; i++)
+        {
+            int ver1 = i<v1.length? Integer.parseInt(v1[i]): 0;
+            int ver2 = i<v2.length? Integer.parseInt(v2[i]): 0;
+            
+            if(ver1> ver2)
+            	return 1;
+            if(ver1 < ver2)
+            	return -1;
+        }
+        return 0;
+    }
+    
+    public static void main(String args[])
+    {
+    	VersionNumbers sol = new VersionNumbers();
+    	System.out.println(sol.compareVersion("1.0.1", "1"));
+    }
+}
