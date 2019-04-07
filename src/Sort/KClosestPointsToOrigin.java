1d7720da25394981572948fad24d005d37ba0777
Priyaranjan1993 <priyaranjan9090@gmail.com>
2019-04-06 21:40:57 -0400

    K closest points to origin

---
+++ src/Sort/KClosestPointsToOrigin.java
@@ -0,0 +1,36 @@
+package Sort;
+
+import java.util.PriorityQueue;
+
+public class KClosestPointsToOrigin {
+
+	public int[][] kClosest(int[][] points, int K) {
+		PriorityQueue<int[]> pq = new PriorityQueue<int[]>((p1,p2) -> p2[0]*p2[0] + p2[1]*p2[1] - p1[0]*p1[0] + p1[1]*p1[1]);
+		for (int[] p : points) {
+			pq.offer(p);
+			if (pq.size() > K) {
+				pq.poll();
+			}
+		}
+		int[][] res = new int[K][2];
+		while (K > 0) {
+			res[--K] = pq.poll();
+		}
+		return res;
+	}
+
+	public static void main(String args[])
+	{
+		
+		KClosestPointsToOrigin obj = new KClosestPointsToOrigin();
+		int K = 2;
+		int[][] points = new int[][] {{3,3},{5,-1},{-2,4}};
+		int[][] res = obj.kClosest(points, K);
+		for(int[] r : res)
+		{
+			System.out.println("***************");
+			System.out.println(r[0]);
+			System.out.println(r[1]);
+		}
+	}
+}
